21af40974819b093b46eac74215854554217443c
faithremo <faithmakumi@gmail.com>
2021-12-12 22:44:35 +0300

    declaring variables

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var Date_of_Birth= document.getElementsById("date");
-var CurrentDate = new Date();
-var Male_Names=["Kwadwo","Kwabena","Kwahu","Yaw","Kofi","Kwame","Kwasi"];
+let akanNames=document.getElementById("akanNames");
+akanNames.addEventListener("submit", (Event)=>{
+    Event.preventDefault();
+
+var Male_Names=["Kwado","Kwabena","Kwahu","Yaw","Kofi","Kwame","Kwasi"];
 var Female_Names=["Adowa","Abenaa","Akua","Yaa","Afua","Ama","Akosua"];
-var Selected_Gender = document.getElementsByName("gender");
+Date_of_Birth=document.getElementById("date");
+Selected_Gender=document.getElementByName("gender");
 
-function validate(){
-    if(document.myForm.year.value == "" || document.myForm.year.value.length !=4 || document.myForm.year.value >2100 || document.myForm.year.value <=1900) {
-        alert( "Please provide a valid year of birth! eg 2019" );
-        document.myForm.year.focus() ;
-        return false;
-     }
 
-}
+
+});
+
+
